a0d126ed7fb33455020ccd620b89f9c0d244a816
AlinaSakharova <75130764+AlinaSakharova@users.noreply.github.com>
2023-09-17 19:07:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -42,4 +42,16 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop:0}, '300');
     });
 });
+$(document).ready(function() {
+    $("a.scroll-to").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
 
+
+});
